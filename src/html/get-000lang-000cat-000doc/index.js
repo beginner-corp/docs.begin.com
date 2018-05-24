60cfcef9279e73a46070cbad744090064acfcf3a
Ryan Block <rblock@gmail.com>
2018-05-24 13:57:03 -0700

    Forgot to update param IDs

--- src/html/get-000lang-000cat-000doc/index.js
+++ src/html/get-000lang-000cat-000doc/index.js
@@ -3,9 +3,9 @@ var render = require('@architect/shared/render')
 
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
-  let lang = req.params.language
-  let doc = req.params.document
-  let cat = req.params.category
+  let lang = req.params.lang
+  let doc = req.params.doc
+  let cat = req.params.cat
   let active = {doc, cat, lang}
   let {html, status} = render(active)
   res({
