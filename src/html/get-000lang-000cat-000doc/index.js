466344da667a52114d1cee0b4e6aa7c1a23a9d67
Ryan Block <rblock@gmail.com>
2018-05-30 13:43:38 -0700

    Rename request 'active' to 'state'
    Will be using active for something else

--- src/html/get-000lang-000cat-000doc/index.js
+++ src/html/get-000lang-000cat-000doc/index.js
@@ -2,12 +2,12 @@ let arc = require('@architect/functions')
 var render = require('@architect/shared/render')
 
 function route(req, res) {
-  console.log(JSON.stringify(req, null, 2))
+  // console.log(JSON.stringify(req, null, 2))
   let lang = req.params.lang
   let doc = req.params.doc
   let cat = req.params.cat
-  let active = {doc, cat, lang}
-  let {html, status} = render(active)
+  let state = {doc, cat, lang}
+  let {html, status} = render(state)
   res({
     status,
     html,
