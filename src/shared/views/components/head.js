1e9dd876628b5be3fed37e9812397f8ff1ef948c
Ryan Block <rblock@gmail.com>
2018-05-30 13:49:36 -0700

    New layout, about to drop all the newness...

---
+++ src/shared/views/components/head.js
@@ -0,0 +1,15 @@
+module.exports = function head (state) {
+  state = state || {}
+  var title = state.title || 'Begin documentation'
+  var description = state.description || 'Begin documentation'
+  return `
+<head>
+  <title>Begin documentation - ${title}</title>
+  <meta name="description" content="${description}"/>
+  <link rel="stylesheet" href="/css/styleguide.css">
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="https://fonts.begin.com/fonts-slate.css">
+  <link rel="stylesheet" href="https://fonts.begin.com/fonts-inconsolata.css">
+</head>
+`
+}
--- src/shared/views/layout.js
+++ src/shared/views/layout.js
@@ -1,16 +1,39 @@
-var head = require('./head')
+var Head = require('./components/head')
+var Header = require('./components/header')
+var Sidebar = require('./components/sidebar')
+var Symbols = require('./components/symbols')
 var md = require('marked')
 
-module.exports = function layout (meta, content) {
+// var script = require('./_script')
+// ${script('menu-toggle')}
 
-  var meta = JSON.parse(meta)
+module.exports = function Layout (state, meta, content, ToC) {
+  state = state || {}
+  meta = meta || {}
+  ToC = ToC || {}
 
   return `
-<html>
-  ${head(meta)}
-  <body>
-    ${md(content)}
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+${Head(meta)}
+<body>
+${Symbols}
+<div class="d-static h-100 o-hidden">
+  <div class="d-flex fd-row fg-1">
+    ${Header()}
+  </div>
+  <div class="d-flex fd-row fg-1">
+    <aside class="sidebar fd-c fg-0 h-100">
+      ${Sidebar(state, ToC)}
+    </aside>
+    <section class="main h-100 fd-c fg-1 pt0" id="doc">
+      <h1 class="fs3">${meta.title}</h1>
+      ${md(content)}
+    </section>
+  </div>
+</div>
+${state.scripts || ''}
+</body>
 </html>
 `
 }
