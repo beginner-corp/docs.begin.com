8b8b07f929c288564114b02ee99407b6bbf758cb
Ryan Block <rblock@gmail.com>
2019-02-03 18:59:55 -0800

    Cleans up old document section generation and really cleans up that ToC hooboy

--- src/shared/views/components/section.js
+++ src/shared/views/components/section.js
@@ -1,34 +1,41 @@
 module.exports = function getSections(ToC, categoryIndex, docIndex) {
   ToC = ToC || {}
 
-  var categoryIndex = categoryIndex
-  var docIndex = docIndex
+  var c = categoryIndex
+  var d = docIndex
 
   // Loop through the doc's sections array, executing Section() to assemble the section list HTML
   let sectionList = []
-  let s = 0
-  for (s = 0; s < ToC[categoryIndex].docs[docIndex].sections.length; s++) {
-    sectionList = sectionList + Section(ToC, categoryIndex, docIndex, s)
+  ToC[c].docs[d].sections.forEach(s => {
+    sectionList.push(Section(s))
+  })
+  
+  // Marked's slugger method
+  //   https://github.com/markedjs/marked/blob/master/lib/marked.js
+  function slug(value) {
+    let slug = value
+      .toLowerCase()
+      .trim()
+      .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g, '')
+      .replace(/\s/g, '-');
+    return '#' + slug
   }
 
   // Assemble the leaf nodes into a list which means we're done now I think?
-  function Section(ToC, categoryIndex, docIndex, s) {
-    let anchor = ToC[categoryIndex].docs[docIndex].sections[s].anchor
-    let name = ToC[categoryIndex].docs[docIndex].sections[s].name
+  function Section(section) {
     return `
       <li class="pb-1 lh0">
         <a
           class="fw-book c-p20 c-h0 c-a0 section-link"
-          href="${anchor}"
+          href="${slug(section)}"
           id="nav-section"
         >
-          ${name}
+          ${section}
         </a>
-      </li>
-    `
+      </li>`
   }
 
   return `
-    <ul id="sections" class="mt-1 pl0">${sectionList}</ul>
+    <ul id="sections" class="mt-1 pl0">${sectionList.join('')}</ul>
 `
 }
