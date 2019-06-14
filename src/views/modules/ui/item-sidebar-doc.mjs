f9b1150f7f7afe154653a6959017a2de6281134c
kj <kristoferjoseph@gmail.com>
2019-06-14 13:04:49 -0700

    Adds section expansion.

--- src/views/modules/ui/item-sidebar-doc.mjs
+++ src/views/modules/ui/item-sidebar-doc.mjs
@@ -1,29 +1,22 @@
 import { Component, html } from '../vendor/preact.mjs'
 import joinClass from '../util/join-classes.mjs'
-const linkClasses = 'mb3 c-p3 c-h0 c-a0 fw-book'
+import SidebarLink from './link-sidebar.mjs'
+const linkClasses = 'c-p3 c-h0 c-a0 fw-book'
 
-class SidebarCategoryItem extends Component {
+class SidebarDocItem extends Component {
   constructor (props) {
     super(props)
-    this.toggle = this.toggle.bind(this)
     this.state = {
       open: props.active || false
     }
   }
 
-  toggle (e) {
-    e.preventDefault()
-    this.setState({
-      open: !this.state.open
-    })
-  }
-
   render (props, state) {
     let active = props.active
+    let href = props.href
     let title = props.title
-    let documents = props.documents
-    let documentsClass = joinClass(
-      'pt-4',
+    let sections = props.sections
+    let docsClass = joinClass(
       'o-hidden',
       'menu-transition',
       'max-h-0',
@@ -36,18 +29,18 @@ class SidebarCategoryItem extends Component {
   active="${active}"
   class="${linkClasses}"
 >
-  <h6
-    class="fs-1 fw-medium c-p8 uppercase nowrap cu-pointer"
-    onclick="${this.toggle}"
+  <${SidebarLink}
+    href="${href}"
+    active="${active}"
   >
     ${title}
-  </h6>
-  <ul class="${documentsClass}">
-    ${documents}
+  <//>
+  <ul class="${docsClass}">
+    ${sections}
   </ul>
 </li>
   `
   }
 }
 
-export default SidebarCategoryItem
+export default SidebarDocItem
--- src/views/modules/ui/sidebar.mjs
+++ src/views/modules/ui/sidebar.mjs
@@ -1,6 +1,7 @@
 import { html } from '../vendor/preact.mjs'
 import SidebarLink from './link-sidebar.mjs'
 import SidebarCategoryItem from './item-sidebar-category.mjs'
+import SidebarDocItem from './item-sidebar-doc.mjs'
 
 export default function Sidebar (props) {
   props = props || {}
@@ -17,50 +18,54 @@ function getCategories (props) {
   props = props || {}
   let active = props.active || {}
   let activeCategory = active.cat || ''
-  let activeDoc = active.doc || ''
   let lang = active.lang || 'en'
   let toc = props.toc || {}
-  return toc.map(c => {
+  // Don't show guides
+  let categories = toc.filter(c => c.catID !== 'guides')
+  return categories.map(c => {
     let category = c.catID || ''
     let active = activeCategory === category
     let title = c.catTitle || ''
     let docs = c.docs
-    let links = getLinks({activeDoc, category, docs, lang})
-    let sections = links && links.length
-      ? html`
-    <ul class="pt-4">
-      ${links}
-    </ul>
-    `
-    : ''
-
+    let documents = getDocs({active: props.active, category, docs, lang})
     return html`
 <${SidebarCategoryItem}
   active="${active}"
-  sections="${sections}"
+  documents="${documents}"
   title="${title}"
 ><//>
     `
   })
 }
 
-function getLinks (props) {
+function getDocs (props) {
   props = props || {}
   let category = props.category || ''
   let docs = props.docs || []
   let lang = props.lang || ''
   return docs.map(d => {
-    let href = `/${lang}/${category}/${d.docID}`
-    let active = props.activeDoc === d.docID
+    let doc = d.docID
+    let href = `/${lang}/${category}/${doc}`
+    let active = props.active.doc === doc
+    let sections = (d.sections || []).map(s => {
+      return html`
+        <li class="ml0">
+          <${SidebarLink}
+            href="${href}"
+          >
+            ${s}
+          <//>
+        </li>
+      `
+    })
+
     return html`
-<li>
-  <${SidebarLink}
-    href="${href}"
+<${SidebarDocItem}
     active="${active}"
-  >
-    ${d.docTitle}
-  <//>
-</li>
+    href="${href}"
+    sections="${sections}"
+    title="${d.docTitle}"
+><//>
   `
   })
 }
