275d39e6224f839f4704c2e051265a090c54a884
Ryan Block <rblock@gmail.com>
2019-07-28 13:41:41 -0700

    Add GA script to home

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -5,6 +5,7 @@ const { html } = imports('@architect/views/modules/vendor/preact.mjs')
 const Learn = imports('@architect/views/modules/pages/learn.mjs').default
 const HTMLDocument = imports('@architect/views/modules/document/html.mjs').default
 const staticAssets = require('@architect/shared/static.json')
+const ThirdParty = require('@architect/views/scripts')
 process.env.STATIC_ASSETS = JSON.stringify(staticAssets)
 
 function route (req, res) {
@@ -38,7 +39,8 @@ function route (req, res) {
       state: {
         account,
         staticAssets
-      }
+      },
+      thirdparty: ThirdParty()
     })
     res({
       html: body
--- src/views/scripts/_ga.js
+++ src/views/scripts/_ga.js
@@ -1,12 +1,10 @@
 module.exports = function ga () {
-
   let env = process.env.NODE_ENV
   let production = 'production'
   let productionId = 1
   let stagingId = 2
 
   function snippet (env) {
-    // console.log('hello')
     return `
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-74655805-${env}"></script>
 <script>
